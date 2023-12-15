504a3268d2cd468fbcda8fce15c7baed0af86afe
Pham Van Viet <vietpham08062003@gmail.com>
2023-12-15 14:18:34 +0700

    ccap nhat 15/12 sap xep lich dat client

--- app/Http/Controllers/Client/API/BookingController.php
+++ app/Http/Controllers/Client/API/BookingController.php
@@ -34,19 +34,20 @@ class BookingController extends Controller
     private $user_phone = '';
     public function index()
     {
-//        $data = $this->model::all();
+        //        $data = $this->model::all();
         $data = User::query()->where('user_type', 'STYLIST')->get();
         return response()->json($data);
     }
     public function timeSheetDetail(string $id)
     {
-        $dataStylist = User::query()->with('timeSheet')->where('id',$id)->first();
+        $dataStylist = User::query()->with('timeSheet')->where('id', $id)->first();
         $dataTimeSheet = Timesheet::all();
-        return response()->json(['dataStylist'=>$dataStylist, 'dataTimeSheet'=>$dataTimeSheet]);
+        return response()->json(['dataStylist' => $dataStylist, 'dataTimeSheet' => $dataTimeSheet]);
     }
 
-    public function randomStylist(){
-        $stylist =User::inRandomOrder()->where('user_type', 'STYLIST')->first();
+    public function randomStylist()
+    {
+        $stylist = User::inRandomOrder()->where('user_type', 'STYLIST')->first();
         return response()->json($stylist);
     }
 
@@ -83,10 +84,10 @@ class BookingController extends Controller
         //Send mail tới stylist khi có đơn hàng mới
         $service = Booking_service::with('service')->where('booking_id', $bookingDone_id)->get();
         $stylist = User::query()->where('id', $request->stylist_id)->first();
-        Mail::to($stylist->email)->queue(new MailStylist($booking,$service));
+        Mail::to($stylist->email)->queue(new MailStylist($booking, $service));
         //end send mail stylist
         $this->sendSms($request->user_phone);
-        return response()->json(['success'=>$bookingDone_id]);
+        return response()->json(['success' => $bookingDone_id]);
     }
 
     public function updateRequest(Request $request, $id)
@@ -119,31 +120,34 @@ class BookingController extends Controller
         $noti = Notification::query()->where('booking_id', $id)->first();
         $noti->delete();
 
-        Mail::to($stylist->email)->queue(new MailStylist($booking,$service));
+        Mail::to($stylist->email)->queue(new MailStylist($booking, $service));
         //end send mail stylist
         $this->sendSms($request->user_phone);
-        return response()->json(['success'=>$id]);
+        return response()->json(['success' => $id]);
     }
 
-    public function stylistDetail(string $id){
-        $data = User::query()->where('id',$id)->first();
+    public function stylistDetail(string $id)
+    {
+        $data = User::query()->where('id', $id)->first();
         return response()->json($data);
     }
 
-    public function bookingDone($id){
-        $data = $this->booking::query()->where('id',$id)->with('service','timeSheet')->first();
+    public function bookingDone($id)
+    {
+        $data = $this->booking::query()->where('id', $id)->with('service', 'timeSheet')->first();
         return response()->json($data);
     }
 
-    public function bookingDestroy($id){
+    public function bookingDestroy($id)
+    {
         $this->booking::where('id', $id)->update(['status' => 0]);
-        $this->booking::where('id',$id)->delete();
-        Notification::where('booking_id',$id)->delete();
-        Booking_service::where('booking_id',$id)->delete();
-        $softDelete = $this->booking::onlyTrashed()->where('id',$id)->first();
+        $this->booking::where('id', $id)->delete();
+        Notification::where('booking_id', $id)->delete();
+        Booking_service::where('booking_id', $id)->delete();
+        $softDelete = $this->booking::onlyTrashed()->where('id', $id)->first();
         if ($softDelete->pttt == 2) {
             $payment = payment::where('booking_id', $id)->first();
-            if($payment && $softDelete->pttt == 2){
+            if ($payment && $softDelete->pttt == 2) {
                 ModelsDestroyBooking::create([
                     'booking_id' => $softDelete->id,
                     'user_phone' => $softDelete->user_phone,
@@ -151,23 +155,29 @@ class BookingController extends Controller
                     'status' => '0'
                 ]);
                 $payment = payment::where('booking_id', $id)->first(); //lấy thông tin payment
-                    Mail::to('pvviet2k3@gmail.com')->queue(new destroyBooking($softDelete, $payment));
+                Mail::to('pvviet2k3@gmail.com')->queue(new destroyBooking($softDelete, $payment));
             }
         }
-        return response()->json(['success'=>'Xóa thành công']);
+        return response()->json(['success' => 'Xóa thành công']);
     }
 
-    function setUserPhone(Request $request){
+    function setUserPhone(Request $request)
+    {
         $user_phone = $request->user_phone;
-        return response()->json(['user_phone'=>$user_phone])->view('client.booking.index');
+        return response()->json(['user_phone' => $user_phone])->view('client.booking.index');
     }
 
-    public function bookingNotification($id){
-        $bookings = Booking::query()->with('timeSheet','stylist')->where('user_id', $id)
-                    ->where('status', 1)
-                    ->whereDoesntHave('results')
-                    ->orderBy('date', 'asc')
-                    ->get();
+    public function bookingNotification($id)
+    {
+        $bookings = Booking::query()
+            ->with(['timeSheet' , 'stylist'])
+            ->where('user_id', $id)
+            ->where('status', 1)
+            ->whereDoesntHave('results')
+            ->orderBy('date', 'asc')
+            ->orderBy('timesheet_id','asc')
+            ->orderBy('created_at', 'asc')
+            ->get();
         return response()->json($bookings);
     }
 
@@ -177,8 +187,8 @@ class BookingController extends Controller
             // Nếu có, loại bỏ tiền tố "+84"
             $phoneNumber = Str::substr($phoneNumber, 3);
         }
-//        $APIKey = "DA549A5A42CFAEA8824C0CE30C0DEF";
-//        $SecretKey = "6302BDD6EE57AB25612AEBDC6CD87E";
+        //        $APIKey = "DA549A5A42CFAEA8824C0CE30C0DEF";
+        //        $SecretKey = "6302BDD6EE57AB25612AEBDC6CD87E";
         $APIKey = "44A12426B71D5CDBD86F3EB12DD2F4";
         $SecretKey = "3FDAB16BC3DBB1DD12814080488663";
         $YourPhone = $phoneNumber;
@@ -200,5 +210,4 @@ class BookingController extends Controller
             Log::info("lỗi  ");
         }
     }
-
 }
